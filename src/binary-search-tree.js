8182b2847cb455a9f7d580fa90a6be4efcee9547
Анастасия Селиванова <selivanova@MacBook-Pro-Anastasia.local>
2025-02-16 22:53:49 +0300

    feat: add 'has' method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,29 +1,79 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  constructor() {
+    this.myRoot = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.myRoot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data);
+    if(!this.myRoot) {
+      this.myRoot = newNode;
+      return;
+    }
+    let currentNode = this.myRoot;
+
+    while (currentNode) {
+      if (newNode.data < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  traverseBFS(callback) {
+    const queue = [this.myRoot];
+
+    while (queue.length) {
+      const node = queue.shift();
+      callback(node);
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+  }
+
+  has(data) {
+    let control = false;
+    this.traverseBFS((node) => {
+      if (node.data === data) {
+        control = true;
+      }
+    })
+    return control;
   }
 
   find(/* data */) {
+    // let control = null;
+    // this.traverseBFS((node) => {
+    //   if (node.data === data) {
+    //     control = data;
+    //   }
+    // })
+    // return control;
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
